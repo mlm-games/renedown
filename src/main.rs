5646b80578845f4f93ca8f469f67cf7686172900
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-06-20 18:14:35 +0530

    fix yml (try wrap main, for cargo run default)

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ mod markdown;
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
-    repose_platform::run_desktop_app(|s, _rc| app::app(s))
+    #[cfg(all(not(target_os = "android"), not(target_arch = "wasm32")))]
+    repose_platform::run_desktop_app(|s, _rc| app::app(s));
+    Ok(())
 }
